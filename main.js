f9f4ebd0216558e05266b8ff08e554463b63e1d1
Kurode <jonatangutierrez116@gmail.com>
2021-04-27 21:03:04 -0400

    refactored because I cant read, it works for anything less than 99 and hundreds

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-const words =['one','two','three','four','five','six','seven','eight',
-'nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen',
-'eigtheen','nineteen'];
+function humanize(num){
+  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+              'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+              'ninety']; //change for ones again
 
-const tens = ['twenty'];
+  var numString = num.toString();
 
-var number, result;
-function NumbersToWords(number){
-    if (number < 0) return 'Not available yet';
-    if (number === 0) return 'Zero';
-    
-    for(i = 0; i < number; i++){
-        if(number < 20)
-        result = words[i];
-        if(number >= 20)
-        result = tens[i];
-    }
+  if (num < 0) throw new Error('Negative numbers are not supported.');
+
+  if (num === 0) return 'zero';
+    var result;
+  //the case of 1 - 20
+  if (num < 20) {
+      result = ones[num];
+      console.log(result);
+    return result;
+  }
+
+  if (numString.length === 2) {
+    result = ones[numString[0]] + ' ' + ones[numString[1]];
     console.log(result);
+    return result;
+  }
+
+  //100 and more
+  if (numString.length == 3) {
+    if (numString[1] === '0' && numString[2] === '0'){
+        result = ones[numString[0]] + ' zero zero';
+        console.log(result);
+        return result;
+    }
+    else{
+        result = ones[numString[0]] + humanize(+(numString[1] + numString[2]));
+        console.log(result);
+        return result;
+    }
+  }
 }
 
-NumbersToWords(15);
+humanize(209);
