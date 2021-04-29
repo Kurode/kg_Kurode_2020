c1d3b5041efb3f1a90668b8cfd2663444dbaf2e9
Kurode <jonatangutierrez116@gmail.com>
2021-04-29 13:33:32 -0400

    Now the program accepts Node.js input, needs refactoring for the logs and array input

--- main.js
+++ main.js
@@ -1,42 +1,30 @@
-function humanize(num){
-  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-              'seventeen', 'eighteen', 'nineteen'];
-  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-              'ninety']; //change for ones again
 
+
+function NumbersToWords(num){
+  var ones = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  var result;
   var numString = num.toString();
 
-  if (num < 0) throw new Error('Negative numbers are not supported.');
-
-  if (num === 0) return 'zero';
-    var result;
-  //the case of 1 - 20
-  if (num < 20) {
+  if (num === 0){
+      console.log(ones[0]);
+  }    
+  
+  if (num < 10) {
       result = ones[num];
       console.log(result);
-    return result;
   }
 
   if (numString.length === 2) {
-    result = ones[numString[0]] + ' ' + ones[numString[1]];
+    result = ones[numString[0]] + ones[numString[1]];
     console.log(result);
-    return result;
   }
 
-  //100 and more
-  if (numString.length == 3) {
-    if (numString[1] === '0' && numString[2] === '0'){
-        result = ones[numString[0]] + ' zero zero';
-        console.log(result);
-        return result;
-    }
-    else{
-        result = ones[numString[0]] + humanize(+(numString[1] + numString[2]));
-        console.log(result);
-        return result;
-    }
+  if (numString.length == 3) {   
+  result = ones[numString[0]] +  ones[numString[1]] + ones[numString[2]];
+  console.log(result);
+    
   }
 }
 
-humanize(209);
+const args = process.argv.slice(2);
+NumbersToWords(args);
