4d53d9f62c12f3082374fbbb94b028d14e209a1f
Kurode <jonatangutierrez116@gmail.com>
2021-04-29 13:53:24 -0400

    implemented array function

--- main.js
+++ main.js
@@ -2,27 +2,27 @@
 
 function NumbersToWords(num){
   var ones = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-  var result;
   var numString = num.toString();
+  const WordsArray = [];
 
   if (num === 0){
-      console.log(ones[0]);
+    WordsArray.push(ones[0]);
+    console.log(WordsArray);
   }    
   
   if (num < 10) {
-      result = ones[num];
-      console.log(result);
+    WordsArray.push(ones[num]);
+    console.log(WordsArray);
   }
 
   if (numString.length === 2) {
-    result = ones[numString[0]] + ones[numString[1]];
-    console.log(result);
+    WordsArray.push(ones[numString[0]] + ones[numString[1]]);
+    console.log(WordsArray);
   }
 
   if (numString.length == 3) {   
-  result = ones[numString[0]] +  ones[numString[1]] + ones[numString[2]];
-  console.log(result);
-    
+    WordsArray.push(ones[numString[0]] +  ones[numString[1]] + ones[numString[2]]);  
+  console.log(WordsArray); 
   }
 }
 
